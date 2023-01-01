ed0843b6e7df4666ff06e92b24eb79c0ed6cb7d6
Soomin <113295627+Soomin-Ji@users.noreply.github.com>
2023-01-02 00:26:54 +0900

    Add files via upload

---
+++ 자료형, 조건문, 반복문 리뷰/Primitive_type.java
@@ -0,0 +1,17 @@
+package week01;
+// 기본 자료형
+public class Primitive_type {
+    public static void main(String[] args) {
+        // write your code here
+        char c = 'A';
+        System.out.println(c); // char 는 글자 하나만
+
+        boolean fact = true;
+        fact = false;
+        System.out.println(fact);
+
+        byte data = 'd'; //d의 아스키코드(ASCII code)를 출력 (d=100)
+        System.out.println(data);
+
+    }
+}
---
+++ 자료형, 조건문, 반복문 리뷰/condition.java
@@ -0,0 +1,26 @@
+package week01;
+public class condition {
+    public static void main(String[] args) {
+        
+        //switch 구문
+        /*char score = 'A';
+        switch (score) {
+            case 'A':
+                System.out.println("A등급 축하합니다");
+                break;
+            case 'B':
+                System.out.println("B등급");
+                break;
+            case 'C':
+                System.out.println("C등급");
+                break;
+            default:
+                System.out.println("C보다 아래 등급입니다");
+        } */
+
+        // 삼항 연산자
+        int a = 5;
+        String result = a < 10 ? "10보다 작습니다." : "10보다 큽니다";
+
+    }
+}
